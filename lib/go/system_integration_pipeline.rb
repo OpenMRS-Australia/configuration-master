15e32cd305f9791f301156057278776bd6855372
Alex Ong <aong@thoughtworks.com>
2012-06-27 01:28:48 +1000

    ...and using the right target this time

--- lib/go/system_integration_pipeline.rb
+++ lib/go/system_integration_pipeline.rb
@@ -7,7 +7,7 @@ module Go
     end
 
     def cpm_module_artifact
-      artifact_location "APP", "package", "target"
+      artifact_location "APP", "package", "omod/target"
     end
 
     def configuration_master_artifact
