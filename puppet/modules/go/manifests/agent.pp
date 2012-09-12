d55e1ae78b212f4230b4330cfe241ddbf480c55e
Alex Ong <aong@thoughtworks.com>
2012-09-12 21:01:56 +1000

    Pete/Alex - fixing functional test targets, adding go profile bits to run functional tests

--- puppet/modules/go/manifests/agent.pp
+++ puppet/modules/go/manifests/agent.pp
@@ -9,12 +9,21 @@ class go::agent {
     source => "http://download01.thoughtworks.com/go/12.2.2/ga/go-agent-12.2.2-15235.noarch.rpm",
     provider => "rpm",
     require => Class["jdk"],
-    notify => Service["go-agent"]
+    notify => Service["go-agent"],
   }
 
   service { "go-agent":
     ensure => "running",
-    require => Package["go-agent"]
+    require => [Package["go-agent"], File["/var/go/.bash_profile"]],
+  }
+
+  file { "/var/go/.bash_profile":
+    ensure => present,
+    owner => "go",
+    group => "go",
+    source => "${work_dir}/modules/go/files/var/go/_bash_profile",
+    require => Package["go-agent"],
+    notify => Service["go-agent"]
   }
 
   file { "/var/go/.ssh":
@@ -27,6 +36,6 @@ class go::agent {
     source => "${work_dir}/modules/go/files/var/go/_ssh/config",
     owner => "go",
     group => "go",
-    mode => 0600
+    mode => 0600,
   }
 }
